b897b35576361aa38fe59cef006cb41b5d568ea3
yoshiInGit <yoshimain016@gmail.com>
2025-07-28 11:00:32 +0900

    firebaseセットアップ

---
+++ app/action/resouce.ts
@@ -0,0 +1,18 @@
+export const addFolder = () =>{
+    
+}
+
+export const changeFolderName = () =>{
+}
+
+export const removeFolder = () =>{
+}
+
+export const addFile = () =>{
+}
+
+export const changeFileName = () =>{
+}
+
+export const removeFile = () =>{
+}
