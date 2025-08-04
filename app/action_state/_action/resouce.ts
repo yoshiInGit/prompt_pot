aa79e1b667ae6ecc8fe11149dcff7062ef6b2817
yoshiInGit <yoshimain016@gmail.com>
2025-08-04 22:41:38 +0900

    actionをリファクター

--- app/action_state/_action/resouce.ts
+++ app/action_state/_action/resouce.ts
@@ -5,145 +5,113 @@ import LoadingState from '../_state/loading_state';
 import ResourceState from '../_state/resouce_state';
 
 export const changeResourceName = async ({resourceId: resource, name}:{resourceId : string, name : string}) =>{   
-    LoadingState.getInstance().isResourceListLoading = true;
-    LoadingState.getInstance().notifyResourceListSub();
+    await _onResourceLoading(async ()=>{
+        // DB更新
+        await updateResourceName({folderId: resource, name: name});
 
-    await updateResourceName({folderId: resource, name: name});
-
-    // ステート更新
-    const resourceState = ResourceState.getInstance();
-    const folderIndex = resourceState.folders.findIndex(folder => folder.id === resource);
-    if (folderIndex !== -1) {
-        resourceState.folders[folderIndex].name = name; // フォルダ名を更新
-        resourceState.notify();
-    }
-    
-    LoadingState.getInstance().isResourceListLoading = false;
-    LoadingState.getInstance().notifyResourceListSub();    
+        // ステート更新
+        const resourceState = ResourceState.getInstance();
+        const folderIndex = resourceState.folders.findIndex(folder => folder.id === resource);
+        if (folderIndex !== -1) {
+            resourceState.folders[folderIndex].name = name; // フォルダ名を更新
+            resourceState.notify();
+        }
+    });
 }
 
 export const restoreFolder = async () => {
-    LoadingState.getInstance().isResourceListLoading = true;
-    LoadingState.getInstance().notifyResourceListSub();
+    await _onResourceLoading(async ()=>{
+        const folders = await getFoldersByParentId("base");
+        const files   = await getFilesByParentId("base"); 
 
-    const folders = await getFoldersByParentId("base");
-    const files   = await getFilesByParentId("base"); 
-
-    const resourceState = ResourceState.getInstance();
-    resourceState.currentFolderId = "base"; // フォルダを復元する場合は、ベースフォルダに戻す
-    resourceState.folders = folders;
-    resourceState.files = files;
-    resourceState.notify();
-
-    LoadingState.getInstance().isResourceListLoading = false;
-    LoadingState.getInstance().notifyResourceListSub();
+        const resourceState = ResourceState.getInstance();
+        resourceState.currentFolderId = "base"; // フォルダを復元する場合は、現在のフォルダをベースフォルダに戻す
+        resourceState.folders = folders;
+        resourceState.files   = files;
+        resourceState.notify();
+    })
 }
 
 export const openFolder = async ({folderId}:{folderId:string}) => {
     const resourceState = ResourceState.getInstance();
 
+    // 誤操作を防ぐために、読み込み中は表示を空にする
     resourceState.folders = [];
-    resourceState.files = [];
+    resourceState.files   = [];
     resourceState.notify();
 
-    LoadingState.getInstance().isResourceListLoading = true;
-    LoadingState.getInstance().notifyResourceListSub();
+    await _onResourceLoading(async()=>{
+        const folders = await getFoldersByParentId(folderId);
+        const files   = await getFilesByParentId(folderId); 
 
-    const folders = await getFoldersByParentId(folderId);
-    const files   = await getFilesByParentId(folderId); 
-
-
-    resourceState.currentFolderId = folderId; // フォルダを復元する場合は、ベースフォルダに戻す
-    resourceState.folders = folders;
-    resourceState.files = files;
-    resourceState.notify();
-    
-
-    LoadingState.getInstance().isResourceListLoading = false;
-    LoadingState.getInstance().notifyResourceListSub();
-
+        resourceState.currentFolderId = folderId; 
+        resourceState.folders = folders;
+        resourceState.files   = files;
+        resourceState.notify();
+    })
 }
 
 export const addFolder = async ({currentFolderId, name}:{currentFolderId:string, name:string}) =>{
-    LoadingState.getInstance().isResourceListLoading = true;
-    LoadingState.getInstance().notifyResourceListSub();
-    
-    const newFolder : Folder = {
-        id: uuidv4(),
-        name: name 
-    };
+    await _onResourceLoading(async ()=>{
+        const newFolder : Folder = {
+            id:   uuidv4(),
+            name: name 
+        };
 
-    // データベース更新 
-    await addResourceFolder({folder: newFolder, currentFolderId: currentFolderId});
+        // データベース更新 
+        await addResourceFolder({folder: newFolder, currentFolderId: currentFolderId});
 
-    //　ステート更新
-    const resourceState = ResourceState.getInstance();
-    if(currentFolderId == resourceState.currentFolderId){ // 現在のフォルダに追加してるか確認
-        resourceState.folders.push(newFolder);
-        resourceState.notify();
-    }
-
-    LoadingState.getInstance().isResourceListLoading = false;
-    LoadingState.getInstance().notifyResourceListSub();
+        //　ステート更新
+        const resourceState = ResourceState.getInstance();
+        if(currentFolderId == resourceState.currentFolderId){ // 現在のフォルダに追加してるか確認
+            resourceState.folders.push(newFolder);
+            resourceState.notify();
+        }
+    });
 
 }
 
 export const removeFolder = async ({folderId, parentFolderId}:{folderId:string, parentFolderId:string}) =>{
-    LoadingState.getInstance().isResourceListLoading = true;
-    LoadingState.getInstance().notifyResourceListSub();
+    await _onResourceLoading(async()=>{
+        //DB処理
+        await deleteFolder({folderId:folderId, parentFolderId:parentFolderId});
 
-    //DB処理
-    await deleteFolder({folderId:folderId, parentFolderId:parentFolderId});
-
-    //ステート更新
-    const resourceState = ResourceState.getInstance();
-    resourceState.folders = resourceState.folders.filter(folder => folder.id !== folderId);
-    resourceState.notify();
-
-    LoadingState.getInstance().isResourceListLoading = false;
-    LoadingState.getInstance().notifyResourceListSub();
+        //ステート更新
+        const resourceState = ResourceState.getInstance();
+        resourceState.folders = resourceState.folders.filter(folder => folder.id !== folderId);
+        resourceState.notify();
+    })
 }
 
 export const addFile = async({fileName, currentFolderId}:{fileName:string, currentFolderId:string}) =>{
-    const newFile : File = {
-        id: uuidv4(),
-        name: fileName
-    }
+    await _onResourceLoading(async()=>{
+        const newFile : File = {
+            id:   uuidv4(),
+            name: fileName
+        }
 
-    LoadingState.getInstance().isResourceListLoading = true;
-    LoadingState.getInstance().notifyResourceListSub();
+        //DB処理
+        await addResource({file: newFile, currentFolderId: currentFolderId});
 
-    //DB処理
-    await addResource({file: newFile, currentFolderId: currentFolderId});
-
-    //ステート更新
-    const resourceState = ResourceState.getInstance();
-    if(currentFolderId == resourceState.currentFolderId){ // 現在のフォルダに追加してるか確認
-        resourceState.files.push(newFile);
-        resourceState.notify();
-    }
-
-    LoadingState.getInstance().isResourceListLoading = false;
-    LoadingState.getInstance().notifyResourceListSub();
-}
-
-export const changeFileName = () =>{
+        //ステート更新
+        const resourceState = ResourceState.getInstance();
+        if(currentFolderId == resourceState.currentFolderId){ // 現在のフォルダに追加してるか確認
+            resourceState.files.push(newFile);
+            resourceState.notify();
+        }
+    });
 }
 
 export const removeFile = async ({fileId, parentFolderId}:{fileId : string, parentFolderId:string}) =>{
-    LoadingState.getInstance().isResourceListLoading = true;
-    LoadingState.getInstance().notifyResourceListSub();
-
-    //DB処理
-    await deleteFile({fileId:fileId, parentFolderId:parentFolderId});
-
-    //ステート更新
-    const resourceState = ResourceState.getInstance();
-    resourceState.files = resourceState.files.filter(file => file.id !== fileId);
-    resourceState.notify();
-
-    LoadingState.getInstance().isResourceListLoading = false;
-    LoadingState.getInstance().notifyResourceListSub();
+    await _onResourceLoading(async ()=>{
+        //DB処理
+        await deleteFile({fileId:fileId, parentFolderId:parentFolderId});
+        
+        //ステート更新
+        const resourceState = ResourceState.getInstance();
+        ResourceState.getInstance().files = resourceState.files.filter(file => file.id !== fileId);
+        resourceState.notify();
+    })
 }
 
 export const selectFile = async ({file}:{file:File}) => {
@@ -152,4 +120,16 @@ export const selectFile = async ({file}:{file:File}) => {
     const resourceState = ResourceState.getInstance();
     resourceState.selectedResource = resource;
     resourceState.notify();
+}
+
+
+// ヘルパー
+const _onResourceLoading = async(method:()=>Promise<void>) => {
+    LoadingState.getInstance().isResourceListLoading = true;
+    LoadingState.getInstance().notifyResourceListSub();
+
+    await method();
+
+    LoadingState.getInstance().isResourceListLoading = false;
+    LoadingState.getInstance().notifyResourceListSub();
 }
